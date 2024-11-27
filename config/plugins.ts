271393b3b6622ac4e7b03f69201a53988cbce6ee
DatND <datnd@nami.trade>
2024-11-27 15:13:36 +0700

    update

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,19 +1,14 @@
-export default () => ({
-    'preview-button': {
+export default ({ env }) => ({
+  'preview-button': {
     config: {
       contentTypes: [
         {
           uid: 'api::post.post',
           draft: {
-            url: 'http://localhost:3000/api/preview',
-            query: {
-              type: 'post',
-              slug: '{slug}',
-              locale: '{locale}',
-            },
+            url: `${env('URL_PREVIEW', 'http://localhost:3000')}/preview/{id}`,
           },
           published: {
-            url: 'http://localhost:3000/{locale}/{slug}',
+            url: `${env('URL_PREVIEW', 'http://localhost:3000')}/preview/{id}`,
           },
         },
       ],
